1de251210a177d98a064bdbfde0267f3a3ef15fe
phphe <phphe@outlook.com>
2019-04-03 20:06:57 +0800

    fix slot node-inner-back

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -3,7 +3,7 @@
   TreeNode(:data="rootData" :store="store")
     template(slot-scope="props")
       slot(:data="props.data" :store="store" :vm="props.vm")
-    template(v-if="$slots['node-inner-back']" slot="node-inner-back" slot-scope="props")
+    template(v-if="$slots['node-inner-back'] || $scopedSlots['node-inner-back']" slot="node-inner-back" slot-scope="props")
       slot(name="node-inner-back" :style="props.style" :data="props.data" :store="props.store" :vm="props.vm")
 </template>
 
